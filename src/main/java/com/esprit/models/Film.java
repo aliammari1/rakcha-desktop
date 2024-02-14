f226d904300c52c351342712f7c3a0a4a56783e9
Hajlaouiloujain <loujain.hajlaoui@esprit.tn>
2024-02-14 20:57:29 +0100

    crud film

---
+++ src/main/java/com/esprit/models/Film.java
@@ -0,0 +1,122 @@
+package com.esprit.models;
+
+import com.esprit.utils.DataSource;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.InputStream;
+import java.sql.Blob;
+import java.sql.Time;
+
+public class Film {
+    private final String nom;
+    private int id;
+    private Blob image;
+
+    private Time duree;
+
+    private String description;
+
+    private int annederalisation;
+
+    private int idcategory;
+
+    public Film(String nom, Blob image, Time duree, String description, int annederalisation, int idcategory) {
+        this.nom = nom;
+        this.image = image;
+        this.duree = duree;
+        this.description = description;
+        this.annederalisation = annederalisation;
+        this.idcategory = idcategory;
+    }
+
+    public Film(int id, String nom, Blob image, Time duree, String description, int annederalisation, int idcategory) {
+        this.id = id;
+        this.nom = nom;
+        this.image = image;
+        this.duree = duree;
+        this.description = description;
+        this.annederalisation = annederalisation;
+        this.idcategory = idcategory;
+    }
+
+    public Film(String nom, String image_path, Time duree, String description, int annederalisation, int idcategory) {
+        this.nom = nom;
+        File file = new File(image_path);
+        try (InputStream in = new FileInputStream(file)) {
+            this.image = DataSource.getInstance().getConnection().createBlob();
+            this.image.setBinaryStream(1).write(in.readAllBytes());
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+        }
+        this.duree = duree;
+        this.description = description;
+        this.annederalisation = annederalisation;
+        this.idcategory = idcategory;
+    }
+
+    public String getNom() {
+        return nom;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public Blob getImage() {
+        return image;
+    }
+
+    public void setImage(Blob image) {
+        this.image = image;
+    }
+
+    public Time getDuree() {
+        return duree;
+    }
+
+    public void setDuree(Time duree) {
+        this.duree = duree;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public int getAnnederalisation() {
+        return annederalisation;
+    }
+
+    public void setAnnederalisation(int annederalisation) {
+        this.annederalisation = annederalisation;
+    }
+
+    public int getIdcategory() {
+        return idcategory;
+    }
+
+    public void setIdcategory(int idcategory) {
+        this.idcategory = idcategory;
+    }
+
+    @Override
+    public String toString() {
+        return "Film{" +
+                "nom='" + nom + '\'' +
+                ", id=" + id +
+                ", image=" + image +
+                ", duree=" + duree +
+                ", description='" + description + '\'' +
+                ", annederalisation=" + annederalisation +
+                ", idcategory=" + idcategory +
+                '}';
+    }
+}
