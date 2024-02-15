1b0f1db0612c06ade71c447750bc98619544a168
aliammari1 <ali.ammari@esprit.tn>
2024-02-15 05:50:48 +0100

    Users Sort

--- src/main/java/com/esprit/services/UserService.java
+++ src/main/java/com/esprit/services/UserService.java
@@ -16,18 +16,18 @@ public class UserService implements IService<User> {
     Connection con;
 
     public UserService() {
-        con = DataSource.getInstance().getConnection();
+        this.con = DataSource.getInstance().getConnection();
     }
 
     @Override
-    public void create(User user) {
+    public void create(final User user) {
         try {
-            PreparedStatement statement = con.prepareStatement(
+            final PreparedStatement statement = this.con.prepareStatement(
                     "INSERT INTO users (nom,prenom,num_telephone,password,role,adresse,date_de_naissance,email,photo_de_profil) VALUES (?,?,?,?,?,?,?,?,?)");
-            updateAndAddStatementSetter(user, statement);
+            this.updateAndAddStatementSetter(user, statement);
             statement.executeUpdate();
             System.out.println("user was added");
-        } catch (SQLException e) {
+        } catch (final SQLException e) {
             System.out.println(e.getMessage());
         }
     }
@@ -35,92 +35,31 @@ public class UserService implements IService<User> {
     @Override
     public List<User> read() {
         try {
-            List<User> userList = new ArrayList<>();
-            String query = "SELECT * FROM users";
-            PreparedStatement statement = con.prepareStatement(query);
-            ResultSet resultSet = statement.executeQuery();
-            int i = 0;
-            while (resultSet.next()) {
-                String role = resultSet.getString("role");
-                userList.add(
-                        switch (role) {
-                            case "admin":
-                                yield new Admin(
-                                        resultSet.getInt("id"),
-                                        resultSet.getString("nom"),
-                                        resultSet.getString("prenom"),
-                                        resultSet.getInt("num_telephone"),
-                                        resultSet.getString("password"),
-                                        resultSet.getString("role"),
-                                        resultSet.getString("adresse"),
-                                        resultSet.getDate("date_de_naissance"),
-                                        resultSet.getString("email"),
-                                        resultSet.getBlob("photo_de_profil"));
-                            case "client":
-                                yield new Client(
-                                        resultSet.getInt("id"),
-                                        resultSet.getString("nom"),
-                                        resultSet.getString("prenom"),
-                                        resultSet.getInt("num_telephone"),
-                                        resultSet.getString("password"),
-                                        resultSet.getString("role"),
-                                        resultSet.getString("adresse"),
-                                        resultSet.getDate("date_de_naissance"),
-                                        resultSet.getString("email"),
-                                        resultSet.getBlob("photo_de_profil"));
-                            case "responsable de cinema":
-                                yield new Responsable_de_cinema(
-                                        resultSet.getInt("id"),
-                                        resultSet.getString("nom"),
-                                        resultSet.getString("prenom"),
-                                        resultSet.getInt("num_telephone"),
-                                        resultSet.getString("password"),
-                                        resultSet.getString("role"),
-                                        resultSet.getString("adresse"),
-                                        resultSet.getDate("date_de_naissance"),
-                                        resultSet.getString("email"),
-                                        resultSet.getBlob("photo_de_profil"));
-                            case "sponsor":
-                                yield new Sponsor(
-                                        resultSet.getInt("id"),
-                                        resultSet.getString("nom"),
-                                        resultSet.getString("prenom"),
-                                        resultSet.getInt("num_telephone"),
-                                        resultSet.getString("password"),
-                                        resultSet.getString("role"),
-                                        resultSet.getString("adresse"),
-                                        resultSet.getDate("date_de_naissance"),
-                                        resultSet.getString("email"),
-                                        resultSet.getBlob("photo_de_profil"));
-                            default:
-                                throw new IllegalStateException("Unexpected role value: " + role);
-                        });
-                System.out.println(userList.get(i));
-                i++;
-
-            }
-            return userList;
-        } catch (SQLException e) {
+            final List<User> userList = new ArrayList<>();
+            final String query = "SELECT * FROM users";
+            final PreparedStatement statement = this.con.prepareStatement(query);
+            return this.getUsers(userList, statement);
+        } catch (final SQLException e) {
             System.out.println(e.getMessage());
         }
         return null;
     }
 
     @Override
-    public void update(User user) {
+    public void update(final User user) {
         try {
-            PreparedStatement statement = con.prepareStatement(
+            final PreparedStatement statement = this.con.prepareStatement(
                     "UPDATE users SET  nom=?,prenom=?,num_telephone=?,password=?,role=?,adresse=?,date_de_naissance=?,email=?,photo_de_profil=? WHERE id=?");
-            updateAndAddStatementSetter(user, statement);
+            this.updateAndAddStatementSetter(user, statement);
             statement.setInt(10, user.getId());
             statement.executeUpdate();
             System.out.println("user is updated");
-        } catch (SQLException e) {
+        } catch (final SQLException e) {
             System.out.println(e.getMessage());
         }
     }
 
-    private void updateAndAddStatementSetter(User user, PreparedStatement statement) throws SQLException {
+    private void updateAndAddStatementSetter(final User user, final PreparedStatement statement) throws SQLException {
         statement.setString(1, user.getNom());
         statement.setString(2, user.getPrenom());
         statement.setInt(3, user.getNum_telephone());
@@ -133,23 +72,102 @@ public class UserService implements IService<User> {
     }
 
     @Override
-    public void delete(User user) {
+    public void delete(final User user) {
         try {
-            PreparedStatement statement = con.prepareStatement("DELETE FROM users WHERE id = ?");
+            final PreparedStatement statement = this.con.prepareStatement("DELETE FROM users WHERE id = ?");
             statement.setInt(1, user.getId());
             statement.executeUpdate();
             System.out.println("user is deleted");
-        } catch (SQLException e) {
+        } catch (final SQLException e) {
             System.out.println(e.getMessage());
         }
     }
 
-    public void sendMail(String Recipient, String messageToSend) {
+    public void sendMail(final String Recipient, final String messageToSend) {
         UserMail.send(Recipient, messageToSend);
     }
 
     public void generateUserPDF() {
-        UserPDF userPDF = new UserPDF();
-        userPDF.generate(read());
+        final UserPDF userPDF = new UserPDF();
+        userPDF.generate(this.sort("role"));
     }
+
+
+    public List<User> sort(final String Option) {
+        try {
+            final List<User> userList = new ArrayList<>();
+            String query = "SELECT * FROM users ORDER BY " + Option;
+            final PreparedStatement statement = this.con.prepareStatement(query);
+            return this.getUsers(userList, statement);
+        } catch (final SQLException e) {
+            System.out.println(e.getMessage());
+        }
+        return null;
+    }
+
+    private List<User> getUsers(final List<User> userList, final PreparedStatement statement) throws SQLException {
+        final ResultSet resultSet = statement.executeQuery();
+        int i = 0;
+        while (resultSet.next()) {
+            final String role = resultSet.getString("role");
+            userList.add(
+                    switch (role) {
+                        case "admin":
+                            yield new Admin(
+                                    resultSet.getInt("id"),
+                                    resultSet.getString("nom"),
+                                    resultSet.getString("prenom"),
+                                    resultSet.getInt("num_telephone"),
+                                    resultSet.getString("password"),
+                                    resultSet.getString("role"),
+                                    resultSet.getString("adresse"),
+                                    resultSet.getDate("date_de_naissance"),
+                                    resultSet.getString("email"),
+                                    resultSet.getBlob("photo_de_profil"));
+                        case "client":
+                            yield new Client(
+                                    resultSet.getInt("id"),
+                                    resultSet.getString("nom"),
+                                    resultSet.getString("prenom"),
+                                    resultSet.getInt("num_telephone"),
+                                    resultSet.getString("password"),
+                                    resultSet.getString("role"),
+                                    resultSet.getString("adresse"),
+                                    resultSet.getDate("date_de_naissance"),
+                                    resultSet.getString("email"),
+                                    resultSet.getBlob("photo_de_profil"));
+                        case "responsable de cinema":
+                            yield new Responsable_de_cinema(
+                                    resultSet.getInt("id"),
+                                    resultSet.getString("nom"),
+                                    resultSet.getString("prenom"),
+                                    resultSet.getInt("num_telephone"),
+                                    resultSet.getString("password"),
+                                    resultSet.getString("role"),
+                                    resultSet.getString("adresse"),
+                                    resultSet.getDate("date_de_naissance"),
+                                    resultSet.getString("email"),
+                                    resultSet.getBlob("photo_de_profil"));
+                        case "sponsor":
+                            yield new Sponsor(
+                                    resultSet.getInt("id"),
+                                    resultSet.getString("nom"),
+                                    resultSet.getString("prenom"),
+                                    resultSet.getInt("num_telephone"),
+                                    resultSet.getString("password"),
+                                    resultSet.getString("role"),
+                                    resultSet.getString("adresse"),
+                                    resultSet.getDate("date_de_naissance"),
+                                    resultSet.getString("email"),
+                                    resultSet.getBlob("photo_de_profil"));
+                        default:
+                            throw new IllegalStateException("Unexpected role value: " + role);
+                    });
+            System.out.println(userList.get(i));
+            i++;
+
+        }
+        return userList;
+    }
+
 }
