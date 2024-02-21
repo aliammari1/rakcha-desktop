6c76dda29e2e78584376e263c550f36612b95b5c
GhayaSassi <ghayasassi61@gmail.com>
2024-02-21 16:27:39 +0100

    crud cinema

--- src/main/java/com/esprit/controllers/ModifierCinemaController.java
+++ src/main/java/com/esprit/controllers/ModifierCinemaController.java
@@ -1,4 +1,60 @@
 package com.esprit.controllers;
 
-public class ModifierCinemaController {
+import com.esprit.models.Cinema;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.TextField;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+
+import java.io.ByteArrayInputStream;
+import java.io.IOException;
+import java.net.URL;
+import java.sql.Blob;
+import java.sql.SQLException;
+import java.util.ResourceBundle;
+
+import javax.imageio.ImageIO;
+
+public class ModifierCinemaController implements Initializable {
+
+    @FXML
+    private TextField tfNom;
+
+    @FXML
+    private TextField tfAdresse;
+
+    @FXML
+    private ImageView tfLogo;
+
+    private Cinema cinema;
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+        // Initialisation des éléments de l'interface
+    }
+
+    // Méthode pour initialiser les données du cinéma
+    public void initData(Cinema cinema) {
+        this.cinema = cinema;
+        // Pré-remplir les champs avec les données du cinéma
+        tfNom.setText(cinema.getNom());
+        tfAdresse.setText(cinema.getAdresse());
+
+        // Afficher l'image du logo du cinéma s'il existe
+        Blob logoBlob = cinema.getLogo();
+        if (logoBlob != null) {
+            try {
+                byte[] logoBytes = logoBlob.getBytes(1, (int) logoBlob.length());
+                ByteArrayInputStream inputStream = new ByteArrayInputStream(logoBytes);
+                Image image = new Image(inputStream);
+                tfLogo.setImage(image);
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
+        } else {
+            // Afficher une image par défaut si le logo n'est pas défini
+            tfLogo.setImage(new Image(getClass().getResourceAsStream("default_logo.png")));
+        }
+    }
 }
