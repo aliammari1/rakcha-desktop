edabb3ef36dbe42c75f7fa25a51a75a78d381177
GhayaSassi <ghayasassi61@gmail.com>
2024-02-21 09:39:19 +0100

    crud cinema

---
+++ src/main/java/com/esprit/controllers/DashboardResponsableController.java
@@ -0,0 +1,31 @@
+package com.esprit.controllers;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
+public class DashboardResponsableController {
+
+    @FXML
+    void listerCinemas(ActionEvent event) throws IOException {
+
+        // Charger la nouvelle interface ListCinemaAdmin.fxml
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("/ListCinemaResponsable.fxml"));
+        Parent root = loader.load();
+
+        // Créer une nouvelle scène avec la nouvelle interface
+        Scene scene = new Scene(root);
+
+        // Créer une nouvelle fenêtre (stage) et y attacher la scène
+        Stage stage = new Stage();
+        stage.setScene(scene);
+        stage.show();
+
+    }
+
+}
