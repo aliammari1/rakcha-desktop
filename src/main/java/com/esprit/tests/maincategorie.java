09dcd0dbc7c2da66dd2d1c274cdb8efb850c8536
nourheneftaymia <nourhene.ftaymia@esprit.tn>
2024-02-22 10:31:56 +0100

    Merge remote-tracking branch 'origin/gestionSeries' into gestionSeries

--- src/main/java/com/esprit/tests/maincategorie.java
+++ src/main/java/com/esprit/tests/maincategorie.java
@@ -8,6 +8,15 @@ import com.esprit.services.episode.EpisodeService;
 import com.esprit.services.serie.SerieService;
 import com.esprit.utils.DataSource;
 
+import javax.sql.rowset.serial.SerialBlob;
+import javax.sql.rowset.serial.SerialException;
+import java.io.IOException;
+import java.sql.SQLException;
+
+import java.io.File;
+import java.nio.file.Files;
+
+import java.sql.Blob;
 import java.sql.SQLException;
 import java.util.List;
 
@@ -18,9 +27,9 @@ public class maincategorie {
         //
 
 
-       // EpisodeService es = new EpisodeService();
-       // es.create(new episode(3, "Premier episode", 1, 1, null, new Serie()));
-       // System.out.println(es.read());
+        // EpisodeService es = new EpisodeService();
+        // es.create(new episode(3, "Premier episode", 1, 1, null, new Serie()));
+        // System.out.println(es.read());
 
 
 //SerieService ss = new SerieService();
@@ -34,25 +43,50 @@ public class maincategorie {
         //System.out.println(ss.read());
         //SerieService ss = new SerieService();
 
-        CategorieService cs = new CategorieService();
+
         SerieService ss = new SerieService();
 
-// Création de la catégorie
-        categorie nouvelleCategorie = new categorie("Drama", "DescriptionCategorie");
-        cs.create(nouvelleCategorie);
 
 // Création de la série avec la catégorie associée
-        serie nouvelleSerie = new serie(0, "Serie1", "Resume", "directeur1", "pays", "image1", nouvelleCategorie);
-        ss.create(nouvelleSerie);
-        System.out.println(ss.read());
+        //CategorieService categorieService = new CategorieService();
+        //categorie categorie = categorieService.getCategorie(5);
+
+        //if (categorie != null && categorie.getNom() != null) {
+        //  serie nouvelleSerie = new serie(0, "nom", "Resume", "directeur1", "pays", "image1", categorie.getIdcategorie());
+        //ss.create(nouvelleSerie);
+        //System.out.println(ss.read());
+        //} else {
+        //  System.out.println("Unable to retrieve the category with ID 5.");
+        //}
+
+        //}
+        //Création d'une nouvelle catégorie sans la récupérer de la base de données
+        categorie nouvelleCategorie = new categorie();
+        nouvelleCategorie.setIdcategorie(5); // Remplacez par l'ID que vous souhaitez
+        nouvelleCategorie.setNom("NomCategorie");
+        nouvelleCategorie.setDescription("DescriptionCategorie");
+        try {
+            File logoFile = new File("C:\\Users\\LENOVO\\Downloads\\421267029_398380526010741_29775440009430911_n.png");
+            byte[] logoBytes = Files.readAllBytes(logoFile.toPath());
+            try {
+                Blob logoBlob = new SerialBlob(logoBytes);
+                serie nouvelleSerie = new serie(0, "NomSerie", "ResumeSerie", "DirecteurSerie", logoBlob, "ImageSerie", nouvelleCategorie.getIdcategorie());
+                ss.create(nouvelleSerie);
+                System.out.println(ss.read());
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
+
+            // Utilisation de la nouvelle catégorie pour créer une série
 
 
-
-
-
+        } catch (IOException e) {
+            System.out.println("Erreur lors de la lecture du fichier logo : " + e.getMessage());
+        }
     }
-
 }
+
+
 
 
 
