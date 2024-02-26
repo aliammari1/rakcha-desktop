d0eeda7e66a9a72c7f15017642e07ba2e7efcc6e
Hajlaouiloujain <loujain.hajlaoui@esprit.tn>
2024-02-26 01:39:51 +0100

    ADD film + Actor jointure

---
+++ src/main/java/com/esprit/services/ActorfilmService.java
@@ -0,0 +1,69 @@
+package com.esprit.services;
+
+import com.esprit.models.Actor;
+import com.esprit.models.Actorfilm;
+import com.esprit.models.Film;
+import com.esprit.utils.DataSource;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+
+public class ActorfilmService implements IService<Actorfilm> {
+    Connection connection;
+
+    public ActorfilmService() {
+        connection = DataSource.getInstance().getConnection();
+    }
+
+    @Override
+    public void create(Actorfilm actorfilm) {
+        FilmService filmService = new FilmService();
+        filmService.create(actorfilm.getIdfilm());
+        String req = "INSERT INTO actorfilm (idfilm,idactor) VALUES (LAST_INSERT_ID(),?)";
+        try {
+            Actor actor = actorfilm.getIdactor();
+            String[] actorNames = actor.getNom().split(", ");
+            PreparedStatement statement = connection.prepareStatement(req);
+            for (String actorname : actorNames) {
+                statement.setInt(1, new ActorService().getActorByNom(actorname).getId());
+                statement.executeUpdate();
+            }
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    @Override
+    public List<Actorfilm> read() {
+        List<Actorfilm> actorfilmArrayList = new ArrayList<>();
+        String req = "SELECT film.*,actor.id,actor.image,actor.biographie, GROUP_CONCAT(actor.nom SEPARATOR ', ') AS ActorNames from actorfilm JOIN actor  ON actorfilm.idactor  = actor.id JOIN film on actorfilm.idfilm  = film.id GROUP BY film.id;";
+        try {
+            PreparedStatement pst = connection.prepareStatement(req);
+            ResultSet rs = pst.executeQuery();
+            // int i = 0;
+            while (rs.next()) {
+                actorfilmArrayList.add(new Actorfilm(new Actor(rs.getInt("actor.id"), rs.getString("actor_names"), rs.getBlob("actor.image"), rs.getString("actor.biographie")), new Film(rs.getInt("film.id"), rs.getNString("film.nom"), rs.getBlob("image"), rs.getTime("duree"), rs.getString("film.description"), rs.getInt("annederalisation"), rs.getInt("idacteur"), rs.getInt("idcinema"))));
+                //     System.out.println(filmArrayList.get(i));
+                //       i++;
+            }
+        } catch (SQLException e) {
+            System.out.println(e.getMessage());
+        }
+
+        return actorfilmArrayList;
+    }
+
+    @Override
+    public void update(Actorfilm actorfilm) {
+
+    }
+
+    @Override
+    public void delete(Actorfilm actorfilm) {
+
+    }
+}
