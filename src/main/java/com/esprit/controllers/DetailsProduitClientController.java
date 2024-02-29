cb4643ef4a53d7db8af59863d79a029ed909cbbf
emma zouaoui <emmat'allah.zoauoui@esprit.tn>
2024-02-29 14:14:26 +0100

    recherche

--- src/main/java/com/esprit/controllers/DetailsProduitClientController.java
+++ src/main/java/com/esprit/controllers/DetailsProduitClientController.java
@@ -11,6 +11,7 @@ import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.layout.AnchorPane;
@@ -36,8 +37,8 @@ public class DetailsProduitClientController implements Initializable {
 
 @FXML
     public FlowPane detailtFlowPane;
-@FXML
-    public AnchorPane homeAnchorPane;
+  @FXML
+    public TextField SearchBar;
 
 
     private int produitId;
