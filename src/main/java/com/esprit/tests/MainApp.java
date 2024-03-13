3ef9650122cbc8d23b97594c7cff11d5bdb3f27e
Hajlaouiloujain <loujain.hajlaoui@esprit.tn>
2024-03-13 08:39:46 +0100

    integration finale

--- src/main/java/com/esprit/tests/MainApp.java
+++ src/main/java/com/esprit/tests/MainApp.java
@@ -16,7 +16,7 @@ public class MainApp extends Application {
 
     @Override
     public void start(Stage primaryStage) throws IOException {
-        FXMLLoader loader = new FXMLLoader(getClass().getResource("/InterfaceCategory.fxml"));
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("/SerieClient.fxml"));
         Parent root = loader.load();
         Scene scene = new Scene(root);
         primaryStage.setScene(scene);
